a1444d6ad7995b4373f7ad7793514da5a6c62b3a
kaushikraj8 <kaushik.raj8@gmail.com>
2022-10-09 05:56:36 +0530

    Update streamlit_app.py
    added someting

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My prants healthy diner")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Menu')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
